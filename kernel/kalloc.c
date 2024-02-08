580142f5392113bc6ccd6cecacd4779d5a0750f5
Yohay <yohay.ailon.tevet@gmail.com>
2024-02-08 14:51:06 +0200

    each hart now has its own freelist, and it may borrow from other harts when it runs out

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,12 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmems[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for (int i = 0; i < NCPU; i++) initlock(&kmems[i].lock, "kmem");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +56,32 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+  int cpu = cpuid();
+  pop_off();
+
+  acquire(&kmems[cpu].lock);
+  r->next = kmems[cpu].freelist;
+  kmems[cpu].freelist = r;
+  release(&kmems[cpu].lock);
+}
+
+static void *
+borrow(int cpu) {
+  struct run *r;
+  for (int i = 0; i < NCPU; i++)
+  {
+    if (i == cpu) continue;
+    acquire(&kmems[i].lock);
+    r = kmems[i].freelist;
+    if (r) {
+      kmems[i].freelist = r->next;
+      release(&kmems[i].lock);
+      return (void *)r;
+    }
+    release(&kmems[i].lock);
+  }
+  return 0;
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +92,17 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+  int cpu = cpuid();
+  pop_off();
+
+  acquire(&kmems[cpu].lock);
+  r = kmems[cpu].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmems[cpu].freelist = r->next;
+  else
+    r = borrow(cpu);
+  release(&kmems[cpu].lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
